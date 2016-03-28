7c6d70685a2ccec0bb58557fd82f826d8b5346c1
omgnull <doomsmile@gmail.com>
2016-03-28 14:57:46 +0300

    test selenium #1

---
+++ config/travis/selenium.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+SELENIUM="selenium-server-standalone-2.53.0.jar"
+SELENIUM_DIR="$HOME/.selenium"
+SELENIUM_DOWNLOAD_URL="http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar"
+
+function download {
+    printf 'Downloading Selenium server $SELENIUM_DOWNLOAD_URL\n'
+    wget -O $1 $SELENIUM_DOWNLOAD_URL
+}
+
+# prepare selenium
+mkdir -p $SELENIUM_DIR
+if ! [ -f "$SELENIUM_DIR/$SELENIUM" ] ; then
+    download "$SELENIUM_DIR/$SELENIUM"
+fi
+
+# run selenium
+printf 'Starting Selenium server\n'
+java -jar selenium-server-standalone-2.53.0.jar > /tmp/selenium.log 2> /tmp/selenium.error &
+
+# wait for selenium
+printf 'Waiting Selenium server to load\n'
+until $(curl --output /dev/null --silent --head --fail http://localhost:4444/wd/hub); do
+    printf '.'
+    sleep 1
+done
+printf '\n'
---
+++ config/travis/web-server.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+PHP_VERSION=$(phpenv version-name)
+PHP_BIN="$HOME/.phpenv/versions/$PHP_VERSION/sbin/php-fpm"
+NGINX_DIR="$HOME/nginx"
+
+function replace_vars {
+    sed \
+        -e "s|%NGINX_DIR%|$NGINX_DIR|g" \
+        -e "s|%TRAVIS_BUILD_DIR%|$TRAVIS_BUILD_DIR|g" \
+        $1
+}
+
+# setup php-fpm
+cp "$HOME/.phpenv/versions/$PHP_VERSION/etc/php-fpm.conf.default" "$HOME/.phpenv/versions/$PHP_VERSION/etc/php-fpm.conf"
+echo "short_open_tag = On" >> "$HOME/.phpenv/versions/$PHP_VERSION/etc/php.ini"
+# run php-fpm
+"$PHP_BIN"
+
+# setup nginx
+mkdir -p ${NGINX_DIR}
+mv "$TRAVIS_BUILD_DIR/config/nginx" "$NGINX_DIR/nginx"
+replace_vars "$NGINX_DIR/nginx.conf"
+replace_vars "$NGINX_DIR/conf.d/condom-shop.test.conf"
+
+# run nginx
+nginx -c "$NGINX_DIR/nginx/nginx.conf"
+
